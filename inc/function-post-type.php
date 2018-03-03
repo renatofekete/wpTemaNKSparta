d5fe14267f22982ae2bf823c781e537eb80336ce
Renato <renatofekete.91@gmail.com>
2018-03-04 00:56:05 +0100

    Custom metaboxes

--- inc/function-post-type.php
+++ inc/function-post-type.php
@@ -45,6 +45,49 @@ function nkSpartaTaksonomija() {
 }
 add_action('init', 'nkSpartaTaksonomija');
 
+function igraciMetabox() {
+	// Metabox koji ce sadrzavati polja za ispunjavanje
+	add_meta_box( 'nkSpartaIgraciMetabox', 'Informacije o igracu', 'igraciMetaboxCallback', 'igraci', 'normal', 'high');
+}
+add_action('add_meta_boxes', 'igraciMetabox');
+
+
+// Polja za metabox
+function igraciMetaboxCallback( $post ) {
+	// Za zastitu
+	wp_nonce_field( basename( __FILE__ ), 'igraciMetabox_nonce' );
+	$igraciMetaboxMeta = get_post_meta( $post->ID );
+	// Ime igraca
+	$imeValue = esc_attr($igraciMetaboxMeta['imeIgraca'][0]);
+	$prezimeValue = esc_attr($igraciMetaboxMeta['prezimeIgraca'][0]);
+
+	echo '<div class="igrac-metabox"><label for="imeIgraca">Ime: </label> <input type="text" name="imeIgraca" id="imeIgraca" class="regular-text" value="'. $imeValue .'"/></div>';
+	// Prezime
+	echo '<div class="igrac-metabox"><label for="prezimeIgraca">Prezime: </label> <input type="text" name="prezimeIgraca" id="prezimeIgraca" class="regular-text" value="'. $prezimeValue .'" /></div>';
+	// Datum rodenja
+	echo '<div class="igrac-metabox"><label for="datumRodenjaIgraca">Datum rodenja: </label> <input type="date" name="datumRodenjaIgraca" id="datumRodenjaIgraca" value="" /></div>';	
+}
+
+function igraciMetaboxSave( $post_id ) {
+	// Vudu magija
+
+	$is_autosave = wp_is_post_autosave( $post_id );
+	$is_revision = wp_is_post_revision( $post_id );
+	$is_valid_nonce = ( isset( $_POST['igraciMetabox_nonce'] ) && wp_verify_nonce( $_POST['igraciMetabox_nonce'], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+	if ( $is_autosave || $is_revision || !$is_valid_nonce) {
+		return;
+	}
+
+	if ( isset($_POST['imeIgraca'] ) ) {
+		update_post_meta( $post_id, 'imeIgraca', sanitize_text_field( $_POST['imeIgraca']));
+	}
+	if ( isset($_POST['prezimeIgraca'] ) ) {
+		update_post_meta( $post_id, 'prezimeIgraca', sanitize_text_field( $_POST['prezimeIgraca']));
+	}
+}
+add_action('save_post', 'igraciMetaboxSave' )
+
 
 
  ?>
--- inc/function-scripts.php
+++ inc/function-scripts.php
@@ -7,6 +7,11 @@ function nkSpartaScripts() {
 	wp_enqueue_style( 'main', get_template_directory_uri() . '/css/main.css', array(), '1.0.0', 'all' );
 }
 add_action('wp_enqueue_scripts', 'nkSpartaScripts');
+function nkSpartaAdminScripts() {
+	// Dodavanje admin style-a
+	wp_enqueue_style( 'admin-style', get_template_directory_uri() . '/css/admin-style.css', array(), '1.0.0', 'all' );
 
+}
+add_action('admin_enqueue_scripts', 'nkSpartaAdminScripts');
 
 ?>
